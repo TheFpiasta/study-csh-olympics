f4ac60f7724317be1a28389200a275c15fb0dfa9
jeremymelpitz <j.melpitz@web.de>
2025-10-08 21:29:08 +0200

    graphs page gap bottom
    nerfive zeile zwischen seite und footer weg

--- webapp/src/app/layout.jsx
+++ webapp/src/app/layout.jsx
@@ -29,39 +29,37 @@ export const metadata = {
 export default function RootLayout({children}) {
     return (
         <html lang="en" className={`dark ${geist.variable} ${geistMono.variable}`} suppressHydrationWarning>
-
-        <body className="min-h-screen antialiased bg-white dark:bg-slate-900">
-        <ThemeProvider>
-            <div className="text-gray-900 bg-white dark:bg-slate-900 dark:text-gray-100">
-                {/* <ThemeToggle /> */}
-                {/* <Navigation /> */}
-                {children}
-            </div>
-        </ThemeProvider>
-        <footer>
-            <div
-                className="w-full text-center py-4 text-sm text-gray-500 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900 backdrop-blur-sm">
-                {/*    three columns*/}
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1
-  md:grid-cols-3 gap-4 items-center">
-                    <div className="text-center md:text-left">
-                        <a href="https://github.com/TheFpiasta/study-csh-olympics"
-                           className="hover:underline">
-                            Open Source on GitHub
-                        </a>
+            <body className="min-h-screen antialiased bg-white dark:bg-slate-900">
+                <ThemeProvider>
+                    <div className="text-gray-900 bg-white dark:bg-slate-900 dark:text-gray-100">
+                        {/* <ThemeToggle /> */}
+                        {/* <Navigation /> */}
+                        {children}
                     </div>
-                    <div className="text-center space-x-4">
-                        <a href="/privacy" className="hover:underline">Privacy Policy</a>
-                        <a href="/terms" className="hover:underline">Terms of Service</a>
+                </ThemeProvider>
+                <footer>
+                    <div
+                        className="w-full py-4 text-sm text-center text-gray-500 bg-white border-t border-gray-200 dark:text-gray-400 dark:border-gray-700 dark:bg-slate-900 backdrop-blur-sm">
+                        {/*    three columns*/}
+                        <div className="grid items-center grid-cols-1 gap-4 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 md:grid-cols-3">
+                            <div className="text-center md:text-left">
+                                <a href="https://github.com/TheFpiasta/study-csh-olympics"
+                                className="hover:underline">
+                                    Open Source on GitHub
+                                </a>
+                            </div>
+                            <div className="space-x-4 text-center">
+                                <a href="/privacy" className="hover:underline">Privacy Policy</a>
+                                <a href="/terms" className="hover:underline">Terms of Service</a>
+                            </div>
+                            <div className="text-center md:text-right">
+                                <Link href={"https://github.com/TheFpiasta/study-csh-olympics/issues"}
+                                    className={"hover:underline"}>Issues & Questions</Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className="text-center md:text-right">
-                        <Link href={"https://github.com/TheFpiasta/study-csh-olympics/issues"}
-                              className={"hover:underline"}>Issues & Questions</Link>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        </body>
+                </footer>
+            </body>
         </html>
     );
 }
